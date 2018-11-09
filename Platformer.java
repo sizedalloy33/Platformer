51bbe655ee10817fd7b6c843174cd59e8e4e5a02
sizedalloy33 <hayden.sachedina@gmail.com>
2018-11-09 12:36:38 -0500

    Add files via upload

---
+++ Platformer.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.util.*;
+import usefulstuff.*;
+
+
+public class Platformer
+{
+
+    public static void main (String[] args)
+    {
+
+	JFrame j = new JFrame ("Platformer");
+	j.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	j
+
+    }
+}
