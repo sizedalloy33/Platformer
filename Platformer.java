deaafd59bfc4a884ad9f7499f90bd57c66398baa
jalapeno17 <josephrobertson17.jr@gmail.com>
2018-11-09 23:50:11 -0500

    Add files via upload

--- Platformer.java
+++ Platformer.java
@@ -11,10 +11,43 @@ public class Platformer
 
     public static void main (String[] args)
     {
-
 	JFrame j = new JFrame ("Platformer");
 	j.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-	j
-
+	j.getContentPane ().add (new PlatformerPanel (), BorderLayout.CENTER);
+  j.setSize (800, 500);
+  j.setVisible (true);
     }
+}
+
+class PlatformerPanel extends JPanel implements KeyListener
+{
+
+  public PlatformerPanel ()
+  {
+
+    addKeyListener(this);
+  }
+}
+
+public void paintComponent (Graphics g)
+{
+
+  requestFocus();
+}
+
+public void keyPressed (KeyEvent e)
+{
+
+}
+
+
+public void keyReleased (KeyEvent e)
+{
+
+}
+
+
+public void keyTyped (KeyEvent e)
+{
+  
 }
